0992108d163b8e9e1730be1f97b562c3129d9538
phutri27 <triphu27@gmail.com>
2025-10-19 15:13:50 +0000

    express commit

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-const { createServer } = require('node:http')
 const fs = require('node:fs/promises');
+const express = require("express");
+const { error } = require('node:console');
+const app = express()
+require('dotenv').config({override: true})
 
-const server = createServer((req, res) => {
-    async function renderData(code, link){
-        try{
-            const data = await fs.readFile(link)
-            res.statusCode = code
-            res.setHeader('Content-Type', 'text/html')
-            res.write(data)
-            res.end()
-        } catch (err){
-            console.log(err)
-        }
+app.get("/", async (req, res) => {
+    try{
+        const data = await fs.readFile('./templates/index.html', { encoding: 'utf8' })
+        res.send(data)
+    } catch(err){
+        throw err
     }
+})
 
-    if (req.url === '/'){
-        renderData(200, './templates/index.html')
-    }
-    else if (req.url === '/contact-me'){
-        renderData(200, './templates/contact-me.html')
-    }
-    else if (req.url === '/about'){
-        renderData(200, './templates/about.html')
-    }
-    else{
-        renderData(404, './templates/404.html')
+
+app.get("/about", async (req, res) => {
+    try{
+        const data = await fs.readFile('./templates/about.html', { encoding: 'utf8' })
+        res.send(data)
+    } catch(err){
+        throw err
     }
 
 })
 
-server.listen(8080)
+app.get("/contact-me", async (req, res) => {
+    try{
+        const data = await fs.readFile('./templates/contact-me.html', { encoding: 'utf8' })
+        res.send(data)
+    } catch(err){
+        throw err
+    }
 
+})
+
+app.use(async (req, res, next) => {
+    try{
+        const data = await fs.readFile('./templates/404.html', { encoding: 'utf8' })
+        res.status(404).send(data)
+    } catch(err){
+        throw err
+    }
+})
+
+app.listen(process.env.PORT, (error) => {
+    if (error)
+        throw error
+})
 
